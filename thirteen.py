ab71d51dfcfedf195aa9e61e9ea6a5937e80bd1d
Andrew <abeinstein@uchicago.edu>
2014-03-09 21:06:01 +0100

    Deleting old files

--- thirteen.py
+++
@@ -1,6 +0,0 @@
-input_file = open("thirteen_input.txt", 'r')
-nums = []
-for num in input_file.readlines():
-	nums.append(int(num))
-
-print sum(nums)
--- twenty.py
+++
@@ -1,4 +0,0 @@
-def factorial(n):
-    return reduce(lambda x, y: x*y, xrange(1, n+1))
-
-print sum(map(lambda c: int(c), list(str(factorial(100)))))
